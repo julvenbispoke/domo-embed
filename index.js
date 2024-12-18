cb8467f7ecc4fbfad87e1cadd6b0bcd4cb20668b
julven <julven_condor@yahoo.com>
2024-12-19 05:44:38 +0800

    new version

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-// console.clear()
 let exists = null
 let client = null
-let link = window.location.href;
-let regex = RegExp("/([a-zA-Z0-9]{10})(?:[/?]|$)");
-let m = link.match(regex);
-// console.log({match: m[1]})
-let insertEl = document.querySelector("#ppd")
+let link = null
+let regex = null
+let m = null
+let insertEl = null
+
+
+let getAsin = async () => {
+	 exists = null
+	 client = null
+	 link = window.location.href;
+	 regex = RegExp("/([a-zA-Z0-9]{10})(?:[/?]|$)");
+	 m = link.match(regex);
+	// console.log({match: m[1]})
+	insertEl = document.querySelector("#ppd")
+	return
+}
+
+getAsin()
 
 chrome.storage.onChanged.addListener((changes, area) => {
+	
  	console.log({changes, area})
  	loadClientId()
 });
 
+window.navigation.addEventListener("navigate", async (event) => {
+
+    console.log('location changed!');
+    await getAsin();
+
+    loadClientId()
+})
 // chrome.storage.sync.clear()
 
 
+
+
 const loadClientId = async () => {
+	console.log("load client TOKEN")
 	try {
 		const data = await chrome.storage.sync.get(null);
 		console.log({data})
-		if("clientId" in data && isFinite(data.clientId)) {
-			loadIframe()
+		// if("clientId" in data && isFinite(data.clientId)) {
+		// 	loadIframe(data)
+		// }
+		if("token" in data) {
+			loadIframe(data)
 		}
 		else {
 			document.querySelector("#domo-iframe").remove()
@@ -33,16 +59,18 @@ const loadClientId = async () => {
 	return;
 }
 
-
 loadClientId()
 
+let loadIframe = async(data) => {
 
-let loadIframe = async() => {
-
-	console.log("loading iframe")
-
+	console.log({loadIframe: data})
+	// return
 	// let url = `http://localhost:3000/hello`;
-	let url = "https://jg7u2upo2udattxsajfylgh6ma0uobry.lambda-url.us-east-1.on.aws/hello"
+
+
+	// let url = "https://jg7u2upo2udattxsajfylgh6ma0uobry.lambda-url.us-east-1.on.aws/hello"
+	let url = `https://jg7u2upo2udattxsajfylgh6ma0uobry.lambda-url.us-east-1.on.aws/domo_embed_v2`
+
 	let resp = await fetch(url, {
 		method: "POST",
 		headers: (() => {
@@ -51,29 +79,44 @@ let loadIframe = async() => {
             header.append("Accept", `*/*`);
             return header
         })(),
-		body: JSON.stringify({asin: m[1]})
+		body: JSON.stringify({asin: m[1], token: data.token})
 	})
 
 	resp = await resp.text()
 
 	console.log({resp})
 
+
 	try {
 		console.log("removing document element")
-		document.querySelector('#domo-form').remove()
+		document.querySelector('#domo-iframe').remove()
 	} catch (err) {
 		console.log("ERR "+err)
 	}
 
 	let insertEl = document.querySelector("#ppd")
-	// console.log({insertEl})
 	let newEl = document.createElement('iframe')
 	newEl.id = 'domo-iframe';
-	newEl.srcdoc = resp
-	newEl.style.minWidth = '100%';
+	newEl.style.width = 'clamp(390px, 1000px, 99vw)';
 	newEl.style.minHeight = '600px';
 	newEl.style.border = "none";
 
+	if(resp == 'not found') {
+		
+		// newEl.srcdoc = `<h4>Client does not have this product '${m[1]}'</h4>`
+		newEl.style.minHeight = '300px';
+		newEl.srcdoc =[
+			`<div>`,
+				`<img src="https://uploads-ssl.webflow.com/643612f85ab90f7412a165c7/660502efd5339cffb2efe9d9_email-bispoke-desktop-01-header-logo.jpg" />`,
+			`</div>`,
+			`<p>‘ASIN not found within client data.  Please contact <b>support@biusers.com</b> if this is unexpected.’</p>`
+		].join(" ")
+		insertEl.after(newEl);	
+		return
+	}
+
+	
+	newEl.srcdoc = resp
 	insertEl.after(newEl);	
 
 }
--- init.js
+++ init.js
@@ -1,23 +1,43 @@
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-const Popup = () => {
+const Index = () => {
   const {
-    useEffect,
-    useState
+    useState,
+    useEffect
   } = React;
-  const [clientId, setClientId] = useState("");
-  const [exists, setExists] = useState(null);
+  const [token, setToken] = useState('');
+  const [authorized, setAuthorized] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const submitHandler = /*#__PURE__*/function () {
+  const [error, setError] = useState("");
+  const [initLogin, setInitLogin] = useState(false);
+  const tokenChangeHandler = value => {
+    setToken(value);
+  };
+  const logout = /*#__PURE__*/function () {
     var _ref = _asyncToGenerator(function* () {
-      setError(false);
-      if (["", null, undefined, isNaN, !isFinite].includes(clientId)) {
-        setError(true);
-        return;
+      console.log("logout");
+      try {
+        yield chrome.storage.sync.clear();
+        setAuthorized(null);
+        setToken("");
+        console.log("clear token");
+      } catch (err) {
+        console.log("ERR: " + err);
       }
+    });
+    return function logout() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+  const login = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator(function* () {
+      setError("");
       setLoading(true);
-      let resp = yield fetch(`https://jg7u2upo2udattxsajfylgh6ma0uobry.lambda-url.us-east-1.on.aws/domo_search_client_id`, {
+      setAuthorized(null);
+      console.log({
+        token
+      });
+      let auth = yield fetch(`https://jg7u2upo2udattxsajfylgh6ma0uobry.lambda-url.us-east-1.on.aws/login_client`, {
         method: "POST",
         headers: (() => {
           const header = new Headers();
@@ -25,102 +45,122 @@ const Popup = () => {
           return header;
         })(),
         body: JSON.stringify({
-          id: clientId
-        })
+          token
+        }),
+        redirect: "follow"
       });
+      auth = yield auth.text();
+      console.log({
+        auth
+      });
+      if (auth != 'authorized') {
+        setError("Invalid token");
+        setLoading(false);
+        setAuthorized(false);
+        return;
+      }
+      setAuthorized(true);
       setLoading(false);
-      try {
-        resp = yield resp.json();
-        console.log({
-          resp
-        });
-        if (resp.length > 0) {
-          yield chrome.runtime.sendMessage({
-            clientId
-          }, function (response) {
-            console.log({
-              response
-            });
-            setExists(true);
-          });
-        } else {
-          setError(true);
-          setExists(false);
-        }
-      } catch (err) {
-        console.log("ERR: " + err);
-      }
     });
-    return function submitHandler() {
-      return _ref.apply(this, arguments);
-    };
-  }();
-  const clearIdHandler = /*#__PURE__*/function () {
-    var _ref2 = _asyncToGenerator(function* () {
-      try {
-        yield chrome.storage.sync.clear();
-        setClientId("");
-        setExists(null);
-        console.log("clear client ID");
-      } catch (err) {
-        console.log("ERR: " + err);
-      }
-    });
-    return function clearIdHandler() {
+    return function login() {
       return _ref2.apply(this, arguments);
     };
   }();
-  const getClientId = /*#__PURE__*/function () {
+  const authorization = /*#__PURE__*/function () {
     var _ref3 = _asyncToGenerator(function* () {
       try {
+        yield chrome.storage.sync.set({
+          token
+        }).then(() => {
+          console.log("token is set: " + token);
+        });
+      } catch (err) {
+        console.log("ERR: " + err);
+      }
+    });
+    return function authorization() {
+      return _ref3.apply(this, arguments);
+    };
+  }();
+  const initLoginHandler = /*#__PURE__*/function () {
+    var _ref4 = _asyncToGenerator(function* () {
+      try {
         const data = yield chrome.storage.sync.get(null);
-        if ("clientId" in data && isFinite(data.clientId)) {
-          setClientId(data.clientId);
+        console.log({
+          data
+        });
+        if ("token" in data) {
+          setToken(data.token);
+          setTimeout(() => {
+            setInitLogin(true);
+          }, 100);
         }
       } catch (err) {
         console.log("ERR: " + err);
       }
     });
-    return function getClientId() {
-      return _ref3.apply(this, arguments);
+    return function initLoginHandler() {
+      return _ref4.apply(this, arguments);
     };
   }();
   useEffect(() => {
-    getClientId();
-  }, []);
-  useEffect(() => {
-    console.log(clientId);
-  }, [clientId]);
-  useEffect(() => {
-    console.log({
-      exists
-    });
-  }, [exists]);
-  return /*#__PURE__*/React.createElement("div", {
-    className: "container-fluid my-2",
-    style: {
-      minWidth: 250
+    if (authorized == true) {
+      console.log(authorized);
+      try {
+        authorization();
+      } catch (err) {
+        console.log("ERR: " + err);
+      }
     }
-  }, /*#__PURE__*/React.createElement("h3", null, "Client ID"), /*#__PURE__*/React.createElement("div", {
-    className: "d-flex gap-1 my-2"
-  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("input", {
-    className: "form-control " + (error ? 'is-invalid' : ''),
-    type: "number",
-    value: clientId,
-    onChange: e => setClientId(e.target.value)
-  })), /*#__PURE__*/React.createElement("button", {
-    className: "btn btn-primary " + (loading ? 'disabled' : ''),
-    onClick: submitHandler
-  }, "Submit"), /*#__PURE__*/React.createElement("button", {
-    className: "btn btn-outline-secondary " + (loading ? 'disabled' : ''),
-    onClick: clearIdHandler
-  }, "Clear")), /*#__PURE__*/React.createElement("p", {
-    className: exists == true ? 'text-success' : exists == false ? 'text-danger' : ''
-  }, exists == true ? 'Client ID exists' : exists == false ? 'Client ID DOES NOT exist' : ''));
+  }, [authorized]);
+  useEffect(() => {
+    if (initLogin) {
+      console.log({
+        initLoginToken: token
+      });
+      setInitLogin(false);
+      login();
+    }
+  }, [initLogin]);
+  useEffect(() => {
+    initLoginHandler();
+  }, []);
+  return /*#__PURE__*/React.createElement("div", {
+    style: {
+      maxWidth: 500
+    },
+    className: "container-fluid my-2"
+  }, authorized ? /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", {
+    className: "h3 mb-1"
+  }, "You are authorized"), /*#__PURE__*/React.createElement("div", {
+    className: "d-grid text-wrap"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "alert alert-success py-2 mb-2",
+    role: "alert"
+  }, "Login Success"), /*#__PURE__*/React.createElement("button", {
+    onClick: logout,
+    className: "btn btn-outline-secondary"
+  }, "Logout"))) : /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", {
+    className: "h3"
+  }, "Enter Token"), error != "" ? /*#__PURE__*/React.createElement("div", {
+    className: "alert alert-danger py-2",
+    role: "alert"
+  }, error) : null, /*#__PURE__*/React.createElement("div", {
+    className: "d-grid gap-3 text-wrap\t"
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    className: "form-control",
+    value: token,
+    onChange: e => tokenChangeHandler(e.target.value)
+  }), /*#__PURE__*/React.createElement("button", {
+    disabled: loading,
+    onClick: login,
+    className: "btn btn-success"
+  }, "Login"))));
 };
 try {
   const root = ReactDOM.createRoot(document.getElementById('app'));
-  root.render( /*#__PURE__*/React.createElement(Popup, null));
+  root.render( /*#__PURE__*/React.createElement(Index, null));
 } catch (err) {
   console.log("ERR: " + err);
 }
